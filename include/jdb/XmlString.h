1ed63943bd5b64ad4842e978c407cada4fa2d98a
James Daniel Brandenburg <jdb12@macfrank.rice.edu>
2016-07-15 09:05:24 -0500

    XmlString.h Changed the INFO() --> DEBUG() Changed the order of replacement now : 1) Key-Value 2) ENV 3) config 4) empty str

--- include/jdb/XmlString.h
+++ include/jdb/XmlString.h
@@ -59,58 +59,30 @@ namespace jdb{
 			int stop = s.find( _tkstop, index >= pos ? index : pos);
 			len = stop - index + 1;
 
-			INFO( classname(), "start = " << index );
-			INFO( classname(), "stop = " << stop );
+			DEBUG( classname(), "start = " << index );
+			DEBUG( classname(), "stop = " << stop );
 			
 			if ( -1 == index || -1 == stop || len < 0 ) return rs;
-			INFO( classname(), "substringing" );
+			
 			rs = s.substr( index+1, len-2 ); // just the part inside of the {}
 			return rs;
 		}
 
 		void replace_token( string &_s, string &_key, int index, int len ){
 
-			if ( cfg.exists( _key ) ) {
-				string rv = cfg.getString( _key );
-				_s.replace( index, len, rv );
-			} else if ( kv.count( _key ) >= 1 ){
+			if ( kv.count( _key ) >= 1 ){
 				string rv = kv[ _key ];
 				_s.replace( index, len, rv );
 			} else if ( getenv( _key.c_str() ) ) {
 				string env = getenv( _key.c_str() );
 				_s.replace( index, len, env );
+			} else if ( cfg.exists( _key ) ) {
+				string rv = cfg.getString( _key );
+				_s.replace( index, len, rv );
 			} else {
 				_s.replace( index, len, "" );
 			}
 		}
-
-		// vector<string> find_keys( string s, string _re ){
-		// 	DEBUG( classname(), "Finding interpolation keys in : " << s );
-		// 	vector<string> keys;
-
-		// 	TRegexp e( _re.c_str() );
-		// 	TString str(s.c_str() );
-
-		// 	int pos = 0;
-		// 	int index = -1;
-		// 	int len = 0;
-		// 	while( (index = str.Index( e, &len, pos )) >= pos ){
-
-		// 		INFO( classname(), "index = " << index << " -> " << index + len);
-		// 		pos = index + 1;
-		// 	}
-
-		// 	// smatch m;
-	 //  // 		regex e ( "\\{(.*?)\\}");
-	 //  // 		regex_search( s, m, e );
-		// 	// while ( regex_search (s,m,e) ) {
-		// 	// 	if ( m.size() >= 2 )
-		// 	// 		keys.push_back( m[1] );
-		// 	// 		s = m.suffix().str();
-		// 	// 		DEBUG( classname(), m[1] );
-		// 	// }
-		// 	return keys;
-		// }
 
 		string format( string _s, string _re ="" ){ 
 
@@ -121,40 +93,16 @@ namespace jdb{
 			while ( index >= 0 ){
 
 				replace_token( _s, key, index, len );
-				INFO( classname(), "new : " << _s );
+				DEBUG( classname(), "new : " << _s );
 
 				pos = index+1;
-				cout << "pos = " << pos << endl;
-				cout << key << endl;
+				DEBUG( classname(), "pos = " << pos );
+				DEBUG( classname(), key );
 				key = first_token_at( _s, index, len, pos );
 			}
 			return _s;
 		}
-		// 	DEBUG( classname(), "Formatting : " << _s );
-		// 	string rs = _s;
-		// 	vector<string> keys = find_keys( _s, _re );
-		// 	int nkvs = kv.size();
-		// 	string nullString = "";
 
-		// 	for (string key : keys ){
-		// 		// cout << "Looking for " << key << endl;
-		// 		regex e( "\\{" + key + "\\}" );
-		// 		if ( nkvs >= 1 && kv.count( key ) >= 1 ){
-		// 			string rv = kv[ key ];
-		// 			rs = regex_replace( rs, e, kv[ key ] );
-		// 		} else if ( cfg.exists( key ) ) {
-		// 			string rv = cfg.getString( key );
-		// 			rs = regex_replace( rs, e, rv );
-		// 		} else  if ( getenv( key.c_str() ) ) {
-		// 			string env = getenv( key.c_str() );
-		// 			rs = regex_replace( rs, e, env );
-		// 		} else {
-		// 			rs = regex_replace( rs, e, nullString );
-		// 		}
-		// 	}
-		// 	DEBUG( classname(), "Result : " << rs );
-		// 	return rs;
-		// }
 
 	protected:
 		map<string, string> kv;
